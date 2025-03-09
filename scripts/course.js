1819ad895f09485cbb4e0bde1307dbfb9d697b97
Guilherme <guimargoni@gmail.com>
2025-03-08 22:55:31 -0300

    second commit

--- scripts/course.js
+++ scripts/course.js
@@ -1,22 +1,93 @@
 const courses = [
-    { code: 'CSE 110', category: 'CSE' },
-    { code: 'WDD 130', category: 'WDD' },
-    { code: 'CSE 111', category: 'CSE' },
-    { code: 'CSE 210', category: 'CSE' },
-    { code: 'WDD 131', category: 'WDD' },
-    { code: 'WDD 231', category: 'WDD' }
-];
+    {
+        subject: 'CSE',
+        number: 110,
+        title: 'Introduction to Programming',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
+        technology: [
+            'Python'
+        ],
+        completed: false
+    },
+    {
+        subject: 'WDD',
+        number: 130,
+        title: 'Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+        technology: [
+            'HTML',
+            'CSS'
+        ],
+        completed: false
+    },
+    {
+        subject: 'CSE',
+        number: 111,
+        title: 'Programming with Functions',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
+        technology: [
+            'Python'
+        ],
+        completed: false
+    },
+    {
+        subject: 'CSE',
+        number: 210,
+        title: 'Programming with Classes',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
+        technology: [
+            'C#'
+        ],
+        completed: false
+    },
+    {
+        subject: 'WDD',
+        number: 131,
+        title: 'Dynamic Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: false
+    },
+    {
+        subject: 'WDD',
+        number: 231,
+        title: 'Frontend Web Development I',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
+        technology: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        completed: false
+    }
+]
 
 function displayCourses(filter) {
     const courseList = document.getElementById('course-list');
     courseList.innerHTML = '';
 
-    const filteredCourses = filter ? courses.filter(course => course.category === filter) : courses;
+    const filteredCourses = filter ? courses.filter(course => course.subject === filter) : courses;
 
     filteredCourses.forEach(course => {
         const button = document.createElement('button');
-        button.className = `course ${course.category.toLowerCase()}`;
-        button.textContent = course.code;
+        button.className = `course ${course.subject.toLowerCase()}`;
+        button.textContent = course.number;
         courseList.appendChild(button);
     });
 }
